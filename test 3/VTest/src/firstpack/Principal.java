bf6f435757256f742755d6716b3967a53f0b9a96
Paulbgt <paul.boogaerts@viacesi.fr>
2017-06-18 15:51:26 +0200

    Add files via upload

---
+++ test 3/VTest/src/firstpack/Principal.java
@@ -0,0 +1,16 @@
+package firstpack;
+
+public class Principal {
+
+
+
+	public static void main(String[] args) {
+		
+		Map m1 = new Map();
+		Fenetre fenetre = new Fenetre();
+		fenetre.setVisible(true);
+
+		
+	}
+
+}
