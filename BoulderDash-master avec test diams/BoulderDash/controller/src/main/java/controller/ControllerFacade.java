392e52317b5e194f257be28ea717a8afbf98ad37
MaxenceKruger <MaxenceKruger@users.noreply.github.com>
2017-06-20 12:53:32 +0200

    GiF Diams

---
+++ BoulderDash-master avec test diams/BoulderDash/controller/src/main/java/controller/ControllerFacade.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.sql.SQLException;
+
+
+import model.IModel;
+import view.IView;
+
+/**
+ * <h1>The Class ControllerFacade provides a facade of the Controller component.</h1>
+ *
+ * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @version 1.0
+ */
+public class ControllerFacade implements IController {
+
+    /** The view. */
+    private final IView  view;
+
+    /** The model. */
+    private final IModel model;
+
+    /**
+     * Instantiates a new controller facade.
+     *
+     * @param view
+     *            the view
+     * @param model
+     *            the model
+     */
+    public ControllerFacade(final IView view, final IModel model) {
+        super();
+        this.view = view;
+        this.model = model;
+    }
+
+    /**
+     * Start.
+     * @return 
+     *
+     * @throws SQLException
+     *             the SQL exception
+     */
+    public String[] start(int select) throws SQLException {
+        final String[] examples = this.getModel().getAllExamples(select);
+        return examples;
+    }
+
+    /**
+     * Gets the view.
+     *
+     * @return the view
+     */
+    public IView getView() {
+        return this.view;
+    }
+
+    /**
+     * Gets the model.
+     *
+     * @return the model
+     */
+    public IModel getModel() {
+        return this.model;
+    }
+}
